5cc318ceda55fbb3631955ee9b7f46b57974188d
erfan ashki <erfanbayern111@gmail.com>
2024-11-09 14:24:36 +0330

    add blog app & implement post model

---
+++ blog/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Post(models.Model):
+    STATUS_CONDITION = (
+        ('PUB', 'PUBLISHED'),
+        ('DRF', 'DRAFT')
+    )
+
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
+    status = models.CharField(choices=STATUS_CONDITION, max_length=5)
+
