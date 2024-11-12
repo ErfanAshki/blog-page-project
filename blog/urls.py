cc03ca46b4c5f010bacf6adc9b77e028523541ef
erfan ashki <erfanbayern111@gmail.com>
2024-11-12 18:35:27 +0330

    implement delete view

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import post_list_view, post_detail_view, post_create_view, post_update_view
+from .views import post_list_view, post_detail_view, post_create_view, post_update_view, post_delete_view
 
 urlpatterns = [
     path('list/', post_list_view, name='post_list'),
     path('<int:pk>/', post_detail_view, name='post_detail'),
     path('create/', post_create_view, name='post_create'),
     path('<int:pk>/update/', post_update_view, name='post_update'),
+    path('<int:pk>/delete/', post_delete_view, name='post_delete'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -39,3 +39,14 @@ def post_update_view(request, pk):
 
     return render(request, 'blog/post_create.html', {'form': form})
 
+
+def post_delete_view(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+    if request.method == 'POST':
+        post.delete()
+        return redirect('post_list')
+
+    return render(request, 'blog/post_delete.html', {'post': post})
+
+
