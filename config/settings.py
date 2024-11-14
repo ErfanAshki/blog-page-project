68799d785ba73ba057c3cb39efe146640639d2b6
erfan ashki <erfanbayern111@gmail.com>
2024-11-14 15:47:37 +0330

    configure for heroku

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0cbzanky$yk&6gcac*p336yl=g@-dwwn^cg9ha)+-cs0sxdjsu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
